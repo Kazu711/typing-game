aa3f08543b65cb63f62219dce60ba1a9d51ef55f
Kazu711 <69203169+Kazu711@users.noreply.github.com>
2021-04-05 00:29:10 +0900

    Update and rename temp to temp.py
    92-95にボタンを追加

--- temp.py
+++ temp.py
@@ -89,5 +89,14 @@ class Application(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
+   # btn1 = tk.Button(root,text="start")
+   # btn1.place(x=130,y=120)        
+   # btn2 = tk.Button(root,text="quit")
     Application(master=root)
     root.mainloop()
+
+
+
+
+
+
